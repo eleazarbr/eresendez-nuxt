a593610e435741493b8365051dba5101395ff6ed
Eleazar Resendez <e.resendez@outlook.com>
2020-05-29 16:20:21 -0500

    fix outline

--- components/global/BookOutline.vue
+++ components/global/BookOutline.vue
@@ -9,16 +9,16 @@
       @open="isOpen = index"
     >
       <div slot="trigger" slot-scope="props" class="card-header" role="button">
-        <p class="card-header-title">{{ collapse.title }}</p>
+        <div class="card-header-title">{{ collapse.title }}</div>
         <a class="card-header-icon">
           <b-icon :icon="props.open ? 'menu-down' : 'menu-up'"></b-icon>
         </a>
       </div>
       <div class="card-content">
-        <div class="content">
-          <ul>
+        <div>
+          <ol class="is-lower-roman" style="margin-top: 0;">
             <li v-for="(item, index) in collapse.list" :key="index">{{ item }}</li>
-          </ul>
+          </ol>
         </div>
       </div>
     </b-collapse>
@@ -38,37 +38,37 @@ export default {
 					list: [
 						'PHP en la actualidad',
 						'¿Por qué usar Laravel?',
-            '¡Hola, Mundo!',
-            'Bulma CSS',
-            'Conclusión'
+						'¡Hola, Mundo!',
+						'Bulma CSS',
+						'Conclusión'
 					]
 				},
 				{
 					title: 'Capítulo 2 - Cómo configurar tu primer proyecto',
 					list: [
-            'Los requerimientos del sistema',
-            'Composer',
-            'Instalación de Laravel y Bulma',
-            'Conclusión'
-          ]
+						'Los requerimientos del sistema',
+						'Composer',
+						'Instalación de Laravel y Bulma',
+						'Conclusión'
+					]
 				},
 				{
 					title: 'Capítulo 3 - La estructura de archivos en Laravel y Bulma',
 					list: [
-            'Directorios principales',
-            'Archivos importantes',
-            'La Configuración de Laravel',
-            'Personalización de Bulma',
-            'Conclusión '
-          ]
-        },
-        {
+						'Directorios principales',
+						'Archivos importantes',
+						'La Configuración de Laravel',
+						'Personalización de Bulma',
+						'Conclusión '
+					]
+				},
+				{
 					title: 'Capítulo 4 - El camino con Laravel y Bulma',
 					list: [
-            'Introducción al proyecto del libro',
-            'El patrón de diseño MVC',
-            'Enrutamiento',
-          ]
+						'Introducción al proyecto del libro',
+						'El patrón de diseño MVC',
+						'Enrutamiento'
+					]
 				}
 			]
 		}
