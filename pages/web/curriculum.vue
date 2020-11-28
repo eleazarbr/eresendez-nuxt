75f1fce7819eb6fb5cda05e898041a249f0c0d23
Eleazar Resendez <e.resendez@outlook.com>
2020-11-28 13:05:03 -0600

    add cv file

---
+++ pages/web/curriculum.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="section">
+    <div class="container">
+      
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'curriculum',
+  head() {
+    return {
+      title: "Curriculum",
+    }
+  },
+}
+</script>
