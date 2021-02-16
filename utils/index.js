5eb493f29f723c4ead2ad193e2a365067154aaee
Eleazar Resendez <e.resendez@outlook.com>
2021-02-16 17:44:59 -0600

    add smooth scrolling

--- utils/index.js
+++ utils/index.js
@@ -10,9 +10,9 @@ export function cookieFromRequest (req, key) {
     return
   }
 
-  const cookie = req.headers.cookie.split(';').find(
-    c => c.trim().startsWith(`${key}=`)
-  )
+  const cookie = req.headers.cookie
+    .split(';')
+    .find((c) => c.trim().startsWith(`${key}=`))
 
   if (cookie) {
     return cookie.split('=')[1]
@@ -28,12 +28,12 @@ export function scrollBehavior (to, from, savedPosition) {
   }
 
   let position = {}
-
   if (to.matched.length < 2) {
     position = { x: 0, y: 0 }
-  } else if (to.matched.some(r => r.components.default.options.scrollToTop)) {
+  } else if (to.matched.some((r) => r.components.default.options.scrollToTop)) {
     position = { x: 0, y: 0 }
-  } if (to.hash) {
+  }
+  if (to.hash) {
     position = { selector: to.hash }
   }
 
