caf8425880b2aa379ec9b2be3ecf7ada75651c3a
Eleazar Resendez <e.resendez@outlook.com>
2021-02-16 19:18:48 -0600

    add cv mockup and data

---
+++ content/wiki/continuous-deployment/github-actions.md
@@ -0,0 +1,13 @@
+---
+title: GitHub Actions
+slug: github-actions
+category: continuous-deployment
+---
+
+## Links
+- [Push deploy a Laravel app for free with GitHub Actions][1]
+- [GitHub Action - Install a SSH key][2]
+
+
+[1]:	https://laravel-news.com/push-deploy-with-github-actions
+[2]:	https://github.com/marketplace/actions/install-ssh-key
--- pages/web/curriculum.vue
+++ pages/web/curriculum.vue
@@ -1,17 +1,69 @@
 <template>
   <div class="section">
     <div class="container">
-      
+      <!-- download button -->
+      <div class="buttons is-right">
+        <b-button icon-left="file-pdf" type="is-danger">Download PDF</b-button>
+      </div>
+
+      <!-- CV -->
+      <div id="curriculum">
+        <!-- Heading -->
+        <div class="columns is-centered">
+          <div class="column is-half">
+            <div class="columns is-vcentered is-mobile">
+              <div class="column is-narrow">
+                <figure class="image is-96x96">
+                  <img class="is-rounded" :src="cv.avatar" alt="Image" />
+                </figure>
+              </div>
+              <div class="column">
+                <p class="text-lg">
+                  {{ cv.display_name }}
+                </p>
+                <div class="text-sm">
+                  <p>
+                    {{ cv.current_position[locale] }}
+                  </p>
+                  <div v-for="(website, i) in cv.websites" :key="i" class="leading-3">
+                    <a class="text-gray-500" :href="website.url" target="_blank">
+                      <b-icon size="is-small" :icon="website.icon"></b-icon>
+                      <span>
+                        {{ website.name }}
+                      </span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Content -->
+      </div>
     </div>
   </div>
 </template>
 <script>
+import cv from "~/static/data/cv.json";
 export default {
-  name: 'curriculum',
+  name: "curriculum",
+
   head() {
     return {
       title: "Curriculum",
-    }
+    };
   },
-}
+
+  async asyncData({ params, $content }) {
+    return {
+      cv,
+    };
+  },
+
+  computed: {
+    locale() {
+      return this.$store.getters["lang/locale"];
+    },
+  },
+};
 </script>
