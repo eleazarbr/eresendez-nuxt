146521348168a9224d74b45abc2552532606c005
Eleazar Resendez <e.resendez@outlook.com>
2021-08-21 12:28:05 -0500

    customize bulma variables

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -2,19 +2,47 @@
   <footer class="hero is-small is-black">
     <div class="hero-body">
       <div class="container">
-        <div class="content text-center">
+        <div class="content has-text-centered">
           <!-- Copy -->
-          <p class="is-size-6">Eleazar Reséndez - {{ $moment().year() }}</p>
-          <!-- Social -->
-          <div class="flex justify-center" style="gap: 0.75rem">
-            <a target="_blank" href="https://twitter.com/eleazarbr"
-              ><b-icon style="color: #00acee" size="is-medium" icon="twitter"></b-icon
+          <p class="is-size-6">
+            <span> 2016 – {{ $moment().year() }}. Eleazar Reséndez </span>
+            <br />
+            <span>
+              The source code is licensed
+              <a
+                class="has-text-link"
+                href="https://opensource.org/licenses/mit-license.php"
+                target="_blank"
+              >
+                MIT.
+              </a>
+              The website content is licensed
+              <a
+                class="has-text-link"
+                href="http://creativecommons.org/licenses/by-nc-sa/4.0/"
+                target="_blank"
+              >
+                CC BY NC SA 4.0.
+              </a>
+            </span>
+          </p>
+
+          <!-- Social Icons -->
+          <div class="is-flex is-justify-content-center" style="gap: 0.75rem">
+            <a
+              class="has-text-twitter"
+              target="_blank"
+              href="https://twitter.com/eleazarbr"
+              ><b-icon size="is-medium" icon="twitter"></b-icon
             ></a>
-            <a target="_blank" href="https://github.com/eleazarbr"
+            <a class="has-text-github" target="_blank" href="https://github.com/eleazarbr"
               ><b-icon size="is-medium" icon="github"></b-icon
             ></a>
-            <b-tooltip label="Let's Connect!" type="is-info">
-              <a target="_blank" href="https://linkedin.com/in/eleazarbr"
+            <b-tooltip label="Currently hibernating!" type="is-linkedin">
+              <a
+                class="has-text-linkedin"
+                target="_blank"
+                href="https://linkedin.com/in/eleazarbr"
                 ><b-icon size="is-medium" icon="linkedin"></b-icon
               ></a>
             </b-tooltip>
